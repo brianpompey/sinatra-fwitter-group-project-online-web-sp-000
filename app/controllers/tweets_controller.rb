9fd6226232bf996d31890afd69b14445fe32fe20
Brian Pompey <brianthescholar@gmail.com>
2020-03-06 20:09:13 +0000

    tweet controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -30,7 +30,41 @@ class TweetsController < ApplicationController
     end
   end
 
-  
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find_by(params[:id])
+      erb :'/tweets/show'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find_by(params[:id])
+      if @tweet && @tweet.user == current_user
+        erb :'tweets/edit'
+      else
+        redirect to '/tweets'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to '/tweets/#{params[:id]}/edit'
+    else
+      redirect to '/login'
+    end
+  end
+
+
+
+
+
 
 
 end
