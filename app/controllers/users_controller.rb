9d78ea9d1197e7f2bb98299bb9e1b87d27425f68
Brian Pompey <brianthescholar@gmail.com>
2020-03-06 19:38:57 +0000

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,26 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/login' do
+    if !logged_in
+      erb :'users/login'
+    else
+      redirect to '/tweets'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+    else
+      redirect to '/signup'
+    else
+  end
+
+  get 'logout' do
+    session.destroy
+    redirect to '/login'
+  end
+
 end
